24acb458cc822b99b277e968eceed4e03e449716
Manuel Kiessling <manuel@kiessling.net>
2015-12-26 16:21:52 +0100

    Installing simplecd is so cheap, we can do it at each run

--- puppet/modules/simplecd/manifests/init.pp
+++ puppet/modules/simplecd/manifests/init.pp
@@ -8,7 +8,6 @@ class simplecd {
   exec { "install simplecd":
     command => "/usr/bin/curl https://raw.githubusercontent.com/manuelkiessling/simplecd/master/simplecd.sh -o /opt/simplecd/simplecd.sh",
     require => File["/opt/simplecd"],
-    creates => "/opt/simplecd/simplecd.sh",
   }
 
   file { "/opt/simplecd/simplecd.sh":
