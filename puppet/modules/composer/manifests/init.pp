419fb492a18b1813705ade93bc194c04a94ac349
Manuel Kiessling <manuel@kiessling.net>
2016-04-08 08:32:04 +0200

    Vagrant setup working again.

--- puppet/modules/composer/manifests/init.pp
+++ puppet/modules/composer/manifests/init.pp
@@ -1,9 +1,17 @@
 class composer {
 
-  exec { "composer global install":
-    command => "/usr/bin/curl -sS https://getcomposer.org/installer | /usr/bin/php ; /bin/mv composer.phar /usr/bin/composer",
-    creates => "/usr/bin/composer",
+  file { "/usr/bin/composer-1.0.0.phar":
+    owner   => "root",
+    group   => "root",
+    mode    => 06755,
+    source  => "puppet:///modules/composer/usr/bin/composer-1.0.0.phar",
     require => [ Class["php5"] ],
+  }
+
+  file { "/usr/bin/composer":
+    ensure  => "link",
+    target  => "/usr/bin/composer-1.0.0.phar",
+    require => File["/usr/bin/composer-1.0.0.phar"],
   }
 
 }
