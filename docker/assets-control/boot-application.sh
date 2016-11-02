b4ee7fefffcbf8e8707f911162a32074939ac344
Manuel Kiessling <manuel@kiessling.net>
2016-11-02 09:34:17 +0100

    Assets as symlinks in container.

--- docker/assets-control/boot-application.sh
+++ docker/assets-control/boot-application.sh
@@ -7,8 +7,8 @@ usermod -u 1000 www-data
 cd /opt/journeymonitor/control
 chown -R www-data:www-data var/cache/ var/logs
 sudo -u www-data make php-dependencies
-sudo make js-dependencies
-sudo -u www-data make assets
+make js-dependencies
+sudo -u www-data php bin/console assets:install --symlink
 sudo -u www-data php bin/console doctrine:migrations:migrate --no-interaction
 
 /etc/init.d/rsyslog start
