fac0fcb04de6e31dfe61030daa19f6665ccc10ea
Manuel Kiessling <manuel@kiessling.net>
2017-03-07 18:34:08 +0100

    journeymonitor/monitor#20: Updating selenese-runner-java to 3.2.0.

--- puppet/modules/selenium/manifests/init.pp
+++ puppet/modules/selenium/manifests/init.pp
@@ -14,18 +14,18 @@ class selenium {
     mode   => "0755",
   }
 
-  file { "/opt/selenese-runner-java/selenese-runner-2.13.0.jar":
+  file { "/opt/selenese-runner-java/selenese-runner-3.2.0.jar":
     owner   => "root",
     group   => "root",
     mode    => "0755",
-    source  => "puppet:///modules/selenium/opt/selenese-runner-java/selenese-runner-2.13.0.jar",
+    source  => "puppet:///modules/selenium/opt/selenese-runner-java/selenese-runner-3.2.0.jar",
     require => File["/opt/selenese-runner-java"],
   }
 
   file { "/opt/selenese-runner-java/selenese-runner.jar":
     ensure => "link",
-    target => "/opt/selenese-runner-java/selenese-runner-2.13.0.jar",
-    require => File["/opt/selenese-runner-java/selenese-runner-2.13.0.jar"],
+    target => "/opt/selenese-runner-java/selenese-runner-3.2.0.jar",
+    require => File["/opt/selenese-runner-java/selenese-runner-3.2.0.jar"],
   }
 
 }
