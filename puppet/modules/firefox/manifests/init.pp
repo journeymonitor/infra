f47dce922c8f2ac708a17fc1e25d7a625e582465
Manuel Kiessling <manuel@kiessling.net>
2017-03-11 12:56:05 +0100

    journeymonitor/monitor#22: Upgrading from Firefox 45.7.0 to 47.0.2.

--- puppet/modules/firefox/manifests/init.pp
+++ puppet/modules/firefox/manifests/init.pp
@@ -11,15 +11,15 @@ class firefox {
   }
 
   exec { "download firefox":
-    command => "/usr/bin/curl -o /var/tmp/firefox-45.7.0esr.tar.bz2 https://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/linux-x86_64/en-US/firefox-45.7.0esr.tar.bz2 > /opt/puppet/install/install-firefox.log 2>&1",
-    creates => "/var/tmp/firefox-45.7.0esr.tar.bz2",
+    command => "/usr/bin/curl -o /var/tmp/firefox-47.0.2.tar.bz2 https://ftp.mozilla.org/pub/firefox/releases/47.0.2/linux-x86_64/en-US/firefox-47.0.2.tar.bz2 > /opt/puppet/install/install-firefox.log 2>&1",
+    creates => "/var/tmp/firefox-47.0.2.tar.bz2",
     require => [ Package["curl", "firefox"], File["/opt/puppet/install"] ],
   }
 
   exec { "extract firefox":
     cwd     => "/opt",
-    command => "/bin/rm -rf /opt/firefox && /bin/tar xvfj /var/tmp/firefox-45.7.0esr.tar.bz2",
-    unless  => "/bin/grep '45.7.0' /opt/firefox/application.ini",
+    command => "/bin/rm -rf /opt/firefox && /bin/tar xvfj /var/tmp/firefox-47.0.2.tar.bz2",
+    unless  => "/bin/grep '47.0.2' /opt/firefox/application.ini",
     require => [ Package["bzip2"] ],
   }
 
